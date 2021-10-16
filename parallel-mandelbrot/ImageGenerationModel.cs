972899b762e8052e3ea8b0b9d26cc984eb587725
wozyn <86981324+wozyn@users.noreply.github.com>
2021-10-16 22:19:03 +0300

    updating models

--- parallel-mandelbrot/ImageGenerationModel.cs
+++ parallel-mandelbrot/ImageGenerationModel.cs
@@ -1,27 +1,96 @@
-﻿using System;
+﻿using guiProject;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace parallel_mandelbrot
 {
-    public class ImageGenerationModel
+    public class ImageGenerationModel : ObservableBase
     {
         public int width { get; set; }
         public int height { get; set; }
-        public int x_ThreadCount { get; set; }
-        public int y_ThreadCount { get; set; }
-        public int maxIterations { get; set; }
-        
-        public double maxZoomX { get; set; }
+
+        private int _x_ThreadCount;
+        public int x_ThreadCount { 
+            get
+            {
+                return _x_ThreadCount;
+            }
+            set
+            {
+                _x_ThreadCount = value;
+                RaisePropertyChanged("x_ThreadCount");
+            }
+        }
+
+        private int _y_ThreadCount;
+        public int y_ThreadCount { 
+            get
+            {
+                return _y_ThreadCount;
+            }
+            set
+            {
+                _y_ThreadCount = value;
+                RaisePropertyChanged("y_ThreadCount");
+            }
+        }
+
+        private int _maxIterations;
+        public int maxIterations { 
+            get
+            {
+                return _maxIterations;
+            }
+            set
+            {
+                _maxIterations = value;
+                RaisePropertyChanged("maxIterations");
+            }
+        }
+        public string maxIterationsStr { get
+            {
+                return maxIterations.ToString("0.00");
+            }
+        }
+
+        private double _maxZoomX;
+        public double maxZoomX { 
+            get
+            {
+                return _maxZoomX;
+            }
+            set
+            {
+                _maxZoomX = value;
+                RaisePropertyChanged("maxZoomX");
+                //OnPropertyChanged();
+            }
+        }
         public double minZoomX { 
             get
             {
                 return -1 * maxZoomX;
             }
         }
-        public double maxZoomY { get; set; }
+
+        private double _maxZoomY;
+        public double maxZoomY { 
+            get
+            {
+                return _maxZoomY;
+            }
+            set
+            {
+                _maxZoomY = value;
+                RaisePropertyChanged("maxZoomY");
+                //OnPropertyChanged();
+            }
+        }
         public double minZoomY
         {
             get
