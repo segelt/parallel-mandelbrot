6c435e3916894a3838265c109be3872fb995f0f8
wozyn <86981324+wozyn@users.noreply.github.com>
2021-10-16 23:44:57 +0300

    remove unused code

--- parallel-mandelbrot/Program.cs
+++ parallel-mandelbrot/Program.cs
@@ -8,33 +8,9 @@ namespace parallel_mandelbrot
 {
     class Program
     {
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
-            AsyncHelper asyncHelper = new AsyncHelper();
 
-            var watch = System.Diagnostics.Stopwatch.StartNew();
-
-            ImageGenerationModel inputModel = new ImageGenerationModel
-            {
-                width = 1920,
-                height = 1080,
-                x_ThreadCount = 6,
-                y_ThreadCount = 10,
-                maxIterations = 250,
-                maxZoomX = 3.5,
-                maxZoomY = 3.5
-            };
-
-            byte[,] byteMap = await asyncHelper.PrepareFractalByteMapAsync(inputModel);
-
-            watch.Stop();
-            var elapsedMs = watch.ElapsedMilliseconds;
-
-            Bitmap bitmap = asyncHelper.BitmapFromByteArray(byteMap, inputModel.width, inputModel.height);
-
-            bitmap.Save($"test_{DateTime.Now.ToString("ddMMyyyy_hhmmss")}.png", ImageFormat.Png);
-            Console.WriteLine(elapsedMs);
-            Console.ReadLine();
         }
     }
 }
