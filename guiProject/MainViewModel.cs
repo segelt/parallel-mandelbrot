04c0d51165eb31504c0b713937a67b81f4f16f7a
wozyn <86981324+wozyn@users.noreply.github.com>
2021-10-16 23:36:42 +0300

    bind dynamically generated fractal image to xaml

--- guiProject/MainViewModel.cs
+++ guiProject/MainViewModel.cs
@@ -3,6 +3,8 @@ using parallel_mandelbrot;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,7 +16,6 @@ namespace guiProject
 {
     public class MainViewModel : ViewModelBase
     {
-        private WriteableBitmap writeableBmp;
         public ICommand ButtonCommand { get; set; }
 
         private ImageGenerationModel _inputModel;
@@ -27,6 +28,34 @@ namespace guiProject
             set
             {
                 _inputModel = value;
+            }
+        }
+
+        private byte[,] generatedImage;
+
+        private Bitmap _bitmap;
+        public Bitmap bitmap { 
+            get
+            {
+                return _bitmap;
+            }
+            set
+            {
+                bitmap = value;
+                RaisePropertyChanged("bitmap");
+            }
+        }
+
+        private BitmapImage _bitmapSource;
+        public BitmapImage bitmapSource { 
+            get
+            {
+                return _bitmapSource;
+            }
+            set
+            {
+                _bitmapSource = value;
+                RaisePropertyChanged("bitmapSource");
             }
         }
 
@@ -43,65 +72,28 @@ namespace guiProject
                 maxZoomY = 3.5
             };
 
-            ButtonCommand = new RelayCommand(o => TriggerButtonAction());
+            ButtonCommand = new RelayCommand(async o => await GenerateFractal());
         }
 
-        private void TriggerButtonAction()
+        private async Task GenerateFractal()
         {
-            Console.WriteLine("Button clicked..");
-        }
 
-        //private void Button_Click(object sender, RoutedEventArgs e)
-        //{
-        //    writeableBmp = new WriteableBitmap((int)bitmapCanvas.ActualWidth,
-        //        (int)bitmapCanvas.ActualHeight,
-        //        96,
-        //        96,
-        //        PixelFormats.Rgb24, null);
+            AsyncHelper asyncHelper = new AsyncHelper();
+            generatedImage = await asyncHelper.PrepareFractalByteMapAsync(inputModel);
+            _bitmap = asyncHelper.BitmapFromByteArray(generatedImage, inputModel.width, inputModel.height);
 
-        //    Image generatedImage = new Image { Source = writeableBmp };
-
-        //    bitmapCanvas.Children.Add(generatedImage);
-
-        //}
-
-        private void drawPixel()
-        {
-            try
+            using (MemoryStream memory = new MemoryStream())
             {
-                // Reserve the back buffer for updates.
-                writeableBmp.Lock();
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Png);
+                BitmapImage bitmapimage = new BitmapImage();
+                memory.Position = 0;
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = memory;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.EndInit();
+                bitmapimage.Freeze();
 
-                for (int i = 0; i < 40; i++)
-                {
-                    int row = i;
-                    int column = i;
-                    unsafe
-                    {
-                        // Get a pointer to the back buffer.
-                        IntPtr pBackBuffer = writeableBmp.BackBuffer;
-
-                        // Find the address of the pixel to draw.
-                        pBackBuffer += row * writeableBmp.BackBufferStride;
-                        pBackBuffer += column * 4;
-
-                        // Compute the pixel's color.
-                        int color_data = 255 << 16; // R
-                        color_data |= 128 << 8;   // G
-                        color_data |= 255 << 0;   // B
-
-                        // Assign the color data to the pixel.
-                        *((int*)pBackBuffer) = color_data;
-                    }
-
-                    // Specify the area of the bitmap that changed.
-                    writeableBmp.AddDirtyRect(new Int32Rect(column, row, 1, 1));
-                }
-            }
-            finally
-            {
-                // Release the back buffer and make it available for display.
-                writeableBmp.Unlock();
+                bitmapSource = bitmapimage;
             }
         }
     }
